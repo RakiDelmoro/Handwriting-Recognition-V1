1560055afff1e3784c0422778a533f2281faaeb4
Raki Delmoro <rakidelmoro@gmail.com>
2024-11-24 14:18:47 +0000

    Model setup

---
+++ Model/vision_transformer.py
@@ -0,0 +1,13 @@
+
+
+def transformer_model(feature_size, patch_size, num_attn_heads, depth, mlp_feature_size_ratio):
+    def positional_encoding():
+        pass
+    def cnn_feature_extractor():
+        pass
+    def encoder_self_attention():
+        pass
+    def decoder_self_attention():
+        pass
+    def multi_layer_perceptron():
+        pass
