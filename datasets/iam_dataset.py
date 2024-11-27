04a61654cecb6d235cc1e7ee918e9ea4499f565c
Raki Delmoro <rakidelmoro@gmail.com>
2024-11-27 04:59:45 +0000

    Refactor: dataset, dataloader

--- datasets/iam_dataset.py
+++ datasets/iam_dataset.py
@@ -1,11 +1,11 @@
 import os
-import cupy
+import numpy
 import cv2 as cv
 from datasets.utils import characters_to_ints
 from datasets.data_processing import image_array_processing
 from datasets.utils import START_TOKEN, PAD_TOKEN, END_TOKEN
 
-def image_to_array(folder, image_path, size):
+def image_to_array(folder, image_path, size, patch_width):
     split_image_path = image_path.split('-')
     image_outer_folder = split_image_path[0]
     image_inner_folder = "-".join(split_image_path[:2])
@@ -13,53 +13,49 @@ def image_to_array(folder, image_path, size):
     image_array = cv.imread(complete_image_path)
     if image_array is not None:
         image_array_as_grayscale = cv.cvtColor(image_array, cv.COLOR_RGB2GRAY)
-        return image_array_processing(image_array_as_grayscale, size)
+        return image_array_processing(image_array_as_grayscale, size, patch_width)
     else: return None
 
 def word_to_token_array(text, max_length=19):
     number_of_pad_tokens = max_length - len(text)
     word_as_char_tokens = characters_to_ints(START_TOKEN) + characters_to_ints(text) + characters_to_ints(END_TOKEN)
     if number_of_pad_tokens != 0: word_as_char_tokens.extend(characters_to_ints(PAD_TOKEN) * number_of_pad_tokens)
-    return cupy.array(word_as_char_tokens, dtype=cupy.uint8)
+    return numpy.array(word_as_char_tokens, dtype=numpy.uint8)
 
-def iam_dataset(folder, txt_file, image_size):
+def iam_dataset(folder, txt_file, image_size, patch_width):
     # a01-000u-06-05 ok 159 1910 1839 458 63 NP Exchange - Example of each line data
-    image_arrays = []
-    word_token_arrays = []
+    extracted_data = []
     dataset_text_line = open(os.path.join(folder, txt_file)).read().splitlines()
     for line in dataset_text_line:
         if line[0] == '#':
             continue
-        split_line_data = line.split()
-        data_not_corrupted = split_line_data[1] == 'ok'
+        line_data_as_list = line.split()
+        data_not_corrupted = line_data_as_list[1] == 'ok'
         if data_not_corrupted:
-            image_path = split_line_data[0] + '.png'
-            word_written_in_image = ' '.join(split_line_data[8:])
-            image_array = image_to_array(folder, image_path, image_size)
+            image_path = line_data_as_list[0] + '.png'
+            word_written_in_image = ' '.join(line_data_as_list[8:])
+            image_array = image_to_array(folder, image_path, image_size, patch_width)
             if image_array is None: continue
             if len(word_written_in_image) == 1: continue
             word_as_character_tokens = word_to_token_array(word_written_in_image)
-            image_arrays.append(cupy.array(image_array))
-            word_token_arrays.append(cupy.array(word_as_character_tokens))
-    return image_arrays, word_token_arrays
+            extracted_data.append([image_array, word_as_character_tokens])
+    return extracted_data
 
-def iam_dataloader(folder, text_file, image_size, batch_size=128):
-    image_arrays, word_token_arrays = iam_dataset(folder, text_file, image_size)
-    num_samples = len(image_arrays)
-    i = 0
-    start_index = 0
-    end_index = batch_size
-    batched_image_arrays = []
-    batched_word_token_arrays = []
-    while True:
-        if num_samples == 0: break
-        batch_image_array = cupy.stack(image_arrays[start_index:end_index], axis=0)
-        batch_word_token_array = cupy.stack(word_token_arrays[start_index:end_index], axis=0)
-        batched_image_arrays.append(batch_image_array)
-        batched_word_token_arrays.append(batch_word_token_array)
-        i += 1 
-        start_index = i * batch_size
-        end_index = start_index + batch_size
-        num_samples -= end_index
-        if num_samples < 0: num_samples = 0
-    return batched_image_arrays, batched_word_token_arrays
+def iam_dataloader(dataset, batch_size=128):
+    remaining_samples = len(dataset) % batch_size
+    batched_iterator = len(dataset) // batch_size
+    dataloader = []
+    for i in range(batched_iterator):
+        start_idx = i * batch_size
+        end_idx = start_idx + batch_size
+        pulled_data = dataset[start_idx:end_idx]
+        batched_image_array = numpy.stack([image_arr for image_arr, _ in pulled_data])
+        batched_word_token = numpy.stack([word_token for _, word_token in pulled_data])
+        dataloader.append([batched_image_array, batched_word_token])
+    if remaining_samples != 0:
+        # pulled the last remaining data
+        pulled_remaining_data = dataset[-remaining_samples:]
+        batched_image_array = numpy.stack([image_arr for image_arr, _ in pulled_remaining_data])
+        batched_word_token = numpy.stack([word_token for _, word_token in pulled_remaining_data])
+        dataloader.append([batched_image_array, batched_word_token])
+    return dataloader
