d507ed0343b79c48222a88ea08c150635f08896c
Raki Delmoro <rakidelmoro@gmail.com>
2024-11-30 12:47:53 +0000

    torch test runner file

--- torch_test.py
+++ torch_test.py
@@ -1,15 +1,16 @@
 import torch
-from datasets.iam_dataset import iam_dataset, iam_dataloader
-from torch_model.transformer import ImageEmbeddings, MultiHeadAttention, EncoderMultiLayerPerceptron, Layer, EncoderLayer, MultiLayerPerceptron, Transformer
-from Model.configurations import PATCH_SIZE, NUM_PATCHES, IMAGE_SIZE
+from torch.utils.data import DataLoader
+from datasets.dataset import IAMDataset
+from Model.configurations import IMAGE_SIZE
+from torch_model.transformer import Transformer
+from torch_based_utils import torch_model_runner
 
-
-# dataset = iam_dataset('./datasets/Dataword', 'words.txt', IMAGE_SIZE, patch_width=16)
-# dataloader = iam_dataloader(dataset)
-
-x = torch.randn(128, IMAGE_SIZE[0], IMAGE_SIZE[1])
-y = torch.randint(low=0, high=83, size=(128, 20))
-dataloader = [[x, y]]
-
-loss = Transformer().training_layers(dataloader)
-print(loss)
+def runner():
+    EPOCHS = 10
+    LEARNING_RATE = 0.001
+    MODEL = Transformer()
+    OPTIMIZER = torch.optim.AdamW
+    DATASET = IAMDataset('./datasets/Dataword', 'words.txt', IMAGE_SIZE, patch_width=16)
+    TRAINING_LOADER = DataLoader(dataset=DATASET, batch_size=128, shuffle=True)
+    torch_model_runner(model=MODEL, training_loader=TRAINING_LOADER, optimizer=OPTIMIZER, learning_rate=LEARNING_RATE, epochs=EPOCHS)
+runner()
