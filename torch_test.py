d5691ece40063e800fae38abae73a4c8d8d064aa
Raki Delmoro <rakidelmoro@gmail.com>
2024-12-01 13:19:18 +0000

    torch runner test file

--- torch_test.py
+++ torch_test.py
@@ -1,4 +1,5 @@
 import torch
+from datasets.utils import split_data
 from torch.utils.data import DataLoader
 from datasets.dataset import IAMDataset
 from Model.configurations import IMAGE_SIZE
@@ -6,11 +7,16 @@ from torch_model.transformer import Transformer
 from torch_based_utils import torch_model_runner
 
 def runner():
-    EPOCHS = 10
+    EPOCHS = 50
     LEARNING_RATE = 0.001
+    BATCH_SIZE = 32
     MODEL = Transformer()
     OPTIMIZER = torch.optim.AdamW
     DATASET = IAMDataset('./datasets/Dataword', 'words.txt', IMAGE_SIZE, patch_width=16)
-    TRAINING_LOADER = DataLoader(dataset=DATASET, batch_size=128, shuffle=True)
-    torch_model_runner(model=MODEL, training_loader=TRAINING_LOADER, optimizer=OPTIMIZER, learning_rate=LEARNING_RATE, epochs=EPOCHS)
+    TRAINING_DATASET, VALIDATION_DATASET = split_data(dataset=DATASET, ratio=0.8)
+    TRAINING_LOADER = DataLoader(dataset=TRAINING_DATASET, batch_size=BATCH_SIZE, shuffle=True)
+    VALIDATION_LOADER = DataLoader(dataset=VALIDATION_DATASET, batch_size=BATCH_SIZE, shuffle=True)
+    # TRAINING_LOADER = [[torch.randn(BATCH_SIZE, IMAGE_SIZE[0], IMAGE_SIZE[1]), torch.randint(low=0, high=83, size=(BATCH_SIZE, 32))]] # Test samples 
+    torch_model_runner(model=MODEL, training_loader=TRAINING_LOADER, validation_loader=VALIDATION_LOADER, optimizer=OPTIMIZER, learning_rate=LEARNING_RATE, epochs=EPOCHS)
+
 runner()
