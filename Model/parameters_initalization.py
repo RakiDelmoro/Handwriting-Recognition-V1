98ad2624a45e0f98006ae5954bafc44828734327
Raki Delmoro <rakidelmoro@gmail.com>
2024-12-16 13:16:36 +0000

    Initialize parameters in CPU

--- Model/parameters_initalization.py
+++ Model/parameters_initalization.py
@@ -11,7 +11,7 @@ def linear_initialization(input_feature, output_feature):
     fan_in, _ = torch.nn.init._calculate_fan_in_and_fan_out(weights)
     bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
     torch.nn.init.uniform_(bias, -bound, bound)
-    return cupy.array(weights, dtype=cupy.float32), cupy.array(bias, dtype=cupy.float32)
+    return np.array(weights, dtype=cupy.float32), np.array(bias, dtype=cupy.float32)
 
 def transformer_parameters_initializer(network_feature_size: int, mlp_architecture: list, output_class: list):
     transformer_parameters = {}
@@ -22,48 +22,48 @@ def transformer_parameters_initializer(network_feature_size: int, mlp_architectu
         num_patches = (image_h // patch_h) * (image_w // patch_w)
         patch_feature_size = patch_h * patch_w
         embeddings_parameters = linear_initialization(patch_feature_size, network_feature_size)
-        classification_token_parameters = cupy.zeros((1, num_patches+2, network_feature_size), dtype=np.float32)
-        distillation_token_parameters = cupy.zeros((1, num_patches+2, network_feature_size), np.float32)
-        position_embeddings_parameters = cupy.zeros((1, num_patches+2, network_feature_size), np.float32)
-        parameters = cupy.stack(embeddings_parameters, axis=0)
-        learnable_tokens = cupy.stack([classification_token_parameters, distillation_token_parameters, position_embeddings_parameters], axis=0)
+        classification_token_parameters = np.zeros((1, num_patches+2, network_feature_size), dtype=np.float32)
+        distillation_token_parameters = np.zeros((1, num_patches+2, network_feature_size), np.float32)
+        position_embeddings_parameters = np.zeros((1, num_patches+2, network_feature_size), np.float32)
+        parameters = np.stack(embeddings_parameters, axis=0)
+        learnable_tokens = np.stack([classification_token_parameters, distillation_token_parameters, position_embeddings_parameters], axis=0)
         transformer_parameters['image_embeddings_parameters'] = parameters
         transformer_parameters['learnable_tokens'] = learnable_tokens
 
     def attention_layer_parameters():
-        axons = cupy.zeros(shape=(3, network_feature_size, network_feature_size))
-        dentrites = cupy.zeros(shape=(3, network_feature_size, network_feature_size))
+        axons = np.zeros(shape=(3, network_feature_size, network_feature_size))
+        dentrites = np.zeros(shape=(3, network_feature_size, network_feature_size))
         for each in range(3):
             layer_axons, layer_dentrites = linear_initialization(network_feature_size, network_feature_size)
             axons[each] = layer_axons
             dentrites[each] = layer_dentrites
-        transformer_parameters['attention_parameters'] = cupy.stack([axons, dentrites], axis=0)
+        transformer_parameters['attention_parameters'] = np.stack([axons, dentrites], axis=0)
 
     def encoder_mlp_layer_parameters():
-        axons = cupy.zeros(shape=(3, network_feature_size*MLP_RATIO, network_feature_size*MLP_RATIO))
-        dentrites = cupy.zeros(shape=(3, network_feature_size*MLP_RATIO, network_feature_size*MLP_RATIO))
+        axons = np.zeros(shape=(3, network_feature_size*MLP_RATIO, network_feature_size*MLP_RATIO))
+        dentrites = np.zeros(shape=(3, network_feature_size*MLP_RATIO, network_feature_size*MLP_RATIO))
         for layer_idx in range(len(mlp_architecture)-1):
             input_size = mlp_architecture[layer_idx]
             output_size = mlp_architecture[layer_idx+1]
             layer_axons, layer_dentrites = linear_initialization(input_size, output_size)
             axons[layer_idx, :input_size, :output_size] = layer_axons
             dentrites[layer_idx, :input_size, :output_size] = layer_dentrites
-        transformer_parameters['encoder_mlp_parameters'] = cupy.stack([axons, dentrites], axis=0)
+        transformer_parameters['encoder_mlp_parameters'] = np.stack([axons, dentrites], axis=0)
 
     def mlp_layer_parameters():
-        axons = cupy.zeros(shape=(3, network_feature_size*MLP_RATIO, network_feature_size*MLP_RATIO))
-        dentrites = cupy.zeros(shape=(3, network_feature_size*MLP_RATIO, network_feature_size*MLP_RATIO))
+        axons = np.zeros(shape=(3, network_feature_size*MLP_RATIO, network_feature_size*MLP_RATIO))
+        dentrites = np.zeros(shape=(3, network_feature_size*MLP_RATIO, network_feature_size*MLP_RATIO))
         for layer_idx in range(len(mlp_architecture)-1):
             input_size = mlp_architecture[layer_idx]
             output_size = mlp_architecture[layer_idx+1]
             layer_axons, layer_dentrites = linear_initialization(input_size, output_size)
             axons[layer_idx, :input_size, :output_size] = layer_axons
             dentrites[layer_idx, :input_size, :output_size] = layer_dentrites
-        transformer_parameters['mlp_parameters'] = cupy.stack([axons, dentrites], axis=0)
+        transformer_parameters['mlp_parameters'] = np.stack([axons, dentrites], axis=0)
 
     def output_layer_parameters():
        parameters = linear_initialization(network_feature_size, output_class)
-       transformer_parameters['output_parameters'] = cupy.stack(parameters, axis=0)
+       transformer_parameters['output_parameters'] = np.stack(parameters, axis=0)
 
     # Transformer architecture ordered parameters
     image_embeddings_parameters()
