caf6759cb11bdde8680e82303765b84d9bba88e7
Raki Delmoro <rakidelmoro@gmail.com>
2024-12-16 13:15:20 +0000

    Implement Neuron class object

---
+++ Neural-Network/neuron.py
@@ -0,0 +1,52 @@
+import cupy as c
+import numpy as n
+
+class Neuron:
+    """Neuron class for storing a scalar value and stress"""
+    def __init__(self, value, axons_and_dentrites=(), operation='', device='cpu'):
+        if device == 'cpu': self.value = n.array(value)
+        else: self.value = c.array(value)
+
+        self.stress = .0
+        self.device = device
+        self.axons_and_dentrites = set(axons_and_dentrites)
+        self.backpropagation = lambda: None
+
+    def __add__(self, other_value):
+        other_value = other_value if isinstance(other_value, Neuron) else Neuron(other_value)
+        out = Neuron(self.value + other_value.value, (self, other_value), '+', self.device)
+        def backpropagation():
+            self.stress += out.stress
+            other_value.stress += out.stress
+        out.backpropagation = backpropagation
+        return out
+
+    def __add__(self, other_value):
+        other_value = other_value if isinstance(other_value, Neuron) else Neuron(other_value)
+        out = Neuron(self.value * other_value.value, (self, other_value), '*', self.device)
+        def backpropagation():
+            self.stress += out.stress
+            other_value.stress += out.stress
+        out.backpropagation = backpropagation
+        return out
+
+    def __repr__(self):
+        return f'Neuron(value={self.value})'
+    
+
+# def Neuron(value, axons_and_dentrites=(), operation=''):
+#     '''Neuron calculations'''
+
+#     def add(other_value):
+#         pass
+
+#     def multiply(other_value):
+#         pass
+
+
+input_x = Neuron(1.0)
+axon = Neuron(0.5)
+dentrites = Neuron(-2.0)
+out = input_x + axon
+print(out.value)
+
